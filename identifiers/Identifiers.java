fb3c425b4c279c7d11d6df8a16b187fde08d0dd0
Nitin <45264530+nitin0310@users.noreply.github.com>
2020-03-21 12:19:04 +0530

    Add files via upload

---
+++ identifiers/Identifiers.java
@@ -0,0 +1,16 @@
+package identifiers;
+//package name in small.
+//class name in Camel case.
+public class Identifiers {
+	
+	//main method in all small.
+	public static void main() {
+		//variable name in small.Not start with _, 123, not any keyword etc.
+		int a=10;
+		System.out.println(a);
+	}
+}
+
+//There are total 50 keywords stored in java.
+//3 literals stored in java.
+//so as total 53 reserved words in java not used as identifier.
